0b52c5ac2566bb26de57a33edbb5a20eedd226c2
Jesse <jessev@tuxera.com>
2023-03-07 17:59:02 +0200

    add source

---
+++ src/__tests__/App.spec.ts
@@ -0,0 +1,26 @@
+import { describe, expect, test } from 'vitest';
+import { mount } from '@vue/test-utils';
+import App from '@/App.vue';
+import Navbar from '@/components/Navbar.vue';
+import Races from '@/views/Races.vue';
+
+describe('App.vue', () => {
+  test('renders a title', () => {
+    const wrapper = mount(App);
+    expect(wrapper.get('h1').text()).toBe('Ponyracer');
+  });
+
+  test('renders the navbar', () => {
+    const wrapper = mount(App);
+    const navbar = wrapper.findComponent(Navbar);
+    // Maybe you forgot to add <Navbar/> in your App.vue component
+    expect(navbar.exists()).toBe(true);
+  });
+
+  test('renders the races list', () => {
+    const wrapper = mount(App);
+    const races = wrapper.findComponent(Races);
+    // Maybe you forgot to add <Races/> in your App.vue component
+    expect(races.exists()).toBe(true);
+  });
+});
